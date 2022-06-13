533d6e9e1778748d07032d3a961b6f0a1de170dd
Jorge Ochoa Reyes <jorgeochoareyes@gmail.com>
2022-06-12 17:37:37 -0700

    Clicking areas now updates left card

--- src/components/Mapper.tsx
+++ src/components/Mapper.tsx
@@ -1,9 +1,10 @@
 import { VStack, SlideFade, Box, Image } from "@chakra-ui/react"
 import { animationDelay } from "../Util/constants"
-import ImageMapper from 'react-img-mapper';
+import ImageMapper, { CustomArea } from 'react-img-mapper';
 import data from '../Util/split_map.json';
+import { MapType } from "../types";
 
-export const Mapper = ({map_img}) => {
+export const Mapper = ({map_img, changeArea}) => {
     let mapData = {
         name: "example",
         areas: data
@@ -16,6 +17,7 @@ export const Mapper = ({map_img}) => {
                 responsive={true}
                 parentWidth={650}
                 src={map_img} 
+                onClick={(e: CustomArea & MapType) => changeArea(e.title)}
         />
             
     )
---
+++ src/types.tsx
@@ -0,0 +1,10 @@
+export interface MapType {
+    id: String,
+    fillColor: string,
+    strokeColor: string,
+    title: string,
+    name: string,
+    coords: number[],
+    poly: number[][],
+    shape: string
+}
